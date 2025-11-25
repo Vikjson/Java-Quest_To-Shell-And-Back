c16ebea88353c22b229f3d7cdca8c900b5e867d2
Felix Andersson <felix.andersson@gmail.com>
2025-11-25 14:59:03 +0100

    changed name of teacher class

---
+++ src/main/java/se/yrgo/teachers/Teacher.java
@@ -0,0 +1,32 @@
+package se.yrgo.teachers;
+
+import java.util.*;
+
+public class Teacher {
+
+    private final String name;
+    private final Subject subject;
+    private List<Question> questions;
+
+    public Teacher(String name, Subject subject, List<Question> questions) {
+        this.name = name;
+        this.subject = subject;
+        this.questions = new ArrayList<>(questions);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Subject getSubject() {
+        return subject;
+    }
+
+    public void shuffleQuestions() {
+        Collections.shuffle(questions);
+    }
+
+    public List<Question> getRandomQuestions(int amount) {
+        return questions.subList(0, Math.min(amount, questions.size()));
+    }
+}
