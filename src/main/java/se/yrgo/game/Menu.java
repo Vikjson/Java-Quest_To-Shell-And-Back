d14dabc223bc22ba90f7e502f186911734529908
Viktoria Johansson <viktoria.k.johansson@gmail.com>
2025-11-30 22:58:30 +0100

    updated .gitignore

--- src/main/java/se/yrgo/game/Menu.java
+++ src/main/java/se/yrgo/game/Menu.java
@@ -1,49 +1,55 @@
-import java.sql.SQLOutput;
+package se.yrgo.game;
+
+import se.yrgo.player.*;
+import se.yrgo.teachers.*;
+import se.yrgo.game.Menu;
 import java.util.Scanner;
 
-public class Menu {
-    // private Path rules = new Path("Rules.txt");
-    private String input;
+public class Menu{
     private final Scanner scan;
+
 
     public Menu(Scanner scan){
     this.scan = scan;
     }
+
+
+public void startGame() {
+    Game game = new Game(scan, this);
+    game.startGame();
 }
 
-    public void startGame() {
-        Game.start(scan);
-    }
-
     public void getRules() {
-        System.out.print("\033[H\033[2J"); // Ansi.clearScreen();
+        System.out.print("\033[H\033[2J"); // eller ev testa Ansi.clearScreen();
        System.out.println("Regler");
        System.out.println("Tryck enter för att gå tillbaka till startmenyn");
        scan.nextLine();
         startMenu();
     }
 
-    public void getHighScore() {
-        //Visa lista på highscore -top 3 Krävs ny klass??
-    }
+//    public void getHighScore() {
+//        //Visa lista på highscore -top 3 Krävs ny klass??
+//    }
 
     public void startMenu(){
-
+        while (true) {
         System.out.println("Welcome to $hell!");
-        System.out.println("[1.] Starta spelet");
-        System.out.println("[2.]Läs spelregler");
-        System.out.println("[3.] High score");
-        System.out.println("[4.] Avsluta spelet");
+        System.out.println("[1] Starta spelet");
+        System.out.println("[2] Läs spelregler");
+        System.out.println("[3] Avsluta spelet");
+     //   System.out.println("[4] High score");
         // System.out.println("5. Inställningar");
 
-        input = scan.nextLine();
+      String input = scan.nextLine();
 
         switch (input) {
-            case "1" -> Menu.startGame(); //Clear screen Ansi.clearScreen();
-            case "2" -> Menu.getRules();
-            case "3" -> Menu.getHighScore();
-            case "4" -> System.exit(0);
+            case "1" -> startGame(); //Clear screen Ansi.clearScreen();
+            case "2" -> getRules();
+            case "3" -> System.exit(0);
+       //     case "4" -> getHighScore();
             //  case "5" -> Menu.settings();
-            default -> System.out.println("Skriv in ett val från 1-5");
+            default -> System.out.println("Skriv in ett val från 1-3");
     }
+}
+}
 }
