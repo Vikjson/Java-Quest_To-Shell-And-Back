38b84225f00dc5c5f2cfb8216baac0b702b9c74f
Viktoria Johansson <viktoria.k.johansson@gmail.com>
2025-12-04 13:51:43 +0100

    Updated to latest version of Menu.java

--- src/main/java/se/yrgo/game/Menu.java
+++ src/main/java/se/yrgo/game/Menu.java
@@ -10,46 +10,46 @@ public class Menu{
 
 
     public Menu(Scanner scan){
-    this.scan = scan;
+        this.scan = scan;
     }
 
 
-public void startGame() {
-    Game game = new Game(scan, this);
-    game.startGame();
-}
+    public void startGame() {
+        Game game = new Game(scan, this);
+        game.startGame();
+    }
 
     public void getRules() {
         System.out.print("\033[H\033[2J"); // eller ev testa Ansi.clearScreen();
-       System.out.println("Regler");
-       System.out.println("Tryck enter för att gå tillbaka till startmenyn");
-       scan.nextLine();
+        System.out.println("Regler");
+        System.out.println("Tryck enter för att gå tillbaka till startmenyn");
+        scan.nextLine();
         startMenu();
     }
 
-//    public void getHighScore() {
-//        //Visa lista på highscore -top 3 Krävs ny klass??
-//    }
 
     public void startMenu(){
         while (true) {
-        System.out.println("Welcome to $hell!");
-        System.out.println("[1] Starta spelet");
-        System.out.println("[2] Läs spelregler");
-        System.out.println("[3] Avsluta spelet");
-     //   System.out.println("[4] High score");
-        // System.out.println("5. Inställningar");
+            System.out.println("~~~~~Welcome to $hell! ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            System.out.println(" ");
+            System.out.println("_____Välj ett alternativ:_____");
+            System.out.println(" ");
+            System.out.println("[1] Starta spelet");
+            System.out.println("[2] Läs spelregler");
+            System.out.println("[3] Avsluta spelet");
+            //   System.out.println("[4] High score");
+            // System.out.println("5. Inställningar");
 
-      String input = scan.nextLine();
+            String input = scan.nextLine();
 
-        switch (input) {
-            case "1" -> startGame(); //Clear screen Ansi.clearScreen();
-            case "2" -> getRules();
-            case "3" -> System.exit(0);
-       //     case "4" -> getHighScore();
-            //  case "5" -> Menu.settings();
-            default -> System.out.println("Skriv in ett val från 1-3");
+            switch (input) {
+                case "1" -> startGame(); //Clear screen Ansi.clearScreen();
+                case "2" -> getRules();
+                case "3" -> System.exit(0);
+                //     case "4" -> getHighScore();
+                //  case "5" -> Menu.settings();
+                default -> System.out.println("Skriv in ett val från 1-3");
+            }
+        }
     }
-}
-}
 }
