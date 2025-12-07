c0d1970342ac33eab19278dbb046c9978e06691e
Felix Andersson <felix.andersson@gmail.com>
2025-12-07 14:33:15 +0100

    deleted unnecessary code

--- src/main/java/se/yrgo/teachers/Question.java
+++ src/main/java/se/yrgo/teachers/Question.java
@@ -6,25 +6,5 @@ public record Question(
                 Subject category,
                 int points,
                 String flavorText) {
- 
-        /**
-         * A method that displays all newline sequences as actual line breaks in
-         * String question.
-         * 
-         * @return the text in String question with actual line breaks.
-         */
-        public String displayQuestion() {
-                return question;
-        }
 
-        /**
-         * A method that displays all newline sequences as actual line breaks in
-         * flavorText.
-         * 
-         * @return the text in String flavorText with actual line breaks.
-         */
-        public String displayFlavorText() {
-                return flavorText;
-        }
 }
-
