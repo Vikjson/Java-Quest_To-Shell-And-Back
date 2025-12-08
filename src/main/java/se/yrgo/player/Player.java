eb15100a62137e3fa36ce300cb6368977bfe643b
Tobias Tuveback <ac75159@skola.goteborg.se>
2025-12-08 19:12:53 +0100

    Wrote Javadocs to Player.java

--- src/main/java/se/yrgo/player/Player.java
+++ src/main/java/se/yrgo/player/Player.java
@@ -2,6 +2,11 @@ package se.yrgo.player;
 
 import se.yrgo.game.*;
 
+/**
+ * This class represents the player and all its stats, such as health, knowledge level and name.
+ * From this class the player can lose and gain both health and knowledge, a name can be set and
+ * if the player loses all their health the game will be lost and the player will return to the menu.
+ */
 public class Player {
 
     private String name;
@@ -18,7 +23,6 @@ public class Player {
         this.name = name;
         this.health = MAX_HEALTH;
         this.knowledge = MIN_KNOWLEDGE;
-        this.menu = menu;
     }
 
     public void setName(String name) {
@@ -41,10 +45,22 @@ public class Player {
         return knowledge;
     }
 
+    /**
+     *
+     * @return a string to present the players stats in health and knowledge
+     * to the users interface.
+     */
     public String statsToString() {
         return "Health: " + getHealth() + " Knowledge: " + getKnowledge();
     }
 
+    /**
+     * This method makes it possible for the player to gain knowledge, the amount of knowledge gained is set
+     * by the amount parameter.
+     * If the player gains more knowledge than the maximum amount allowed the knowledge will be set to maximum.
+     *
+     * @param amount of knowledge to be gained.
+     */
     public void gainKnowledge(int amount) {
         knowledge += amount;
         if (knowledge > MAX_KNOWLEDGE) {
@@ -52,6 +68,13 @@ public class Player {
         }
     }
 
+    /**
+     * This method makes it possible for the player to lose knowledge, the amount of knowledge lost is set
+     * by the amount parameter.
+     * If the player loses more knowledge than the minimum amount allowed the knowledge will be set to minimum.
+     *
+     * @param amount of knowledge to be lost.
+     */
     public void loseKnowledge(int amount) {
         knowledge -= amount;
         if (knowledge < MIN_KNOWLEDGE) {
@@ -59,6 +82,13 @@ public class Player {
         }
     }
 
+    /**
+     * This method makes it possible for the player to gain health, the amount of health gained is set
+     * by the amount parameter.
+     * If the player gains more health than the maximum amount allowed the health will be set to maximum.
+     *
+     * @param amount of health to be gained.
+     */
     public void gainHealth(int amount) {
         health += amount;
         if (health > MAX_HEALTH) {
@@ -66,6 +96,13 @@ public class Player {
         }
     }
 
+    /**
+     * This method makes it possible for the player to lose health, the amount of health lost is set
+     * by the amount parameter.
+     * If the player loses more health than the minimum amount allowed the health will be set to minimum.
+     *
+     * @param amount of health to be lost.
+     */
     public void loseHealth(int amount) {
         health -= amount;
         if (health <= MIN_HEALTH) {
